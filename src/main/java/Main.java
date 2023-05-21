7a5e8ab968b98d8a4bcf160301819f6ebc5cf609
mabuttta <mabutta@gmail.com>
2023-05-21 11:42:02 +0300

    ver 1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+       if (!(Files.exists(Path.of("readme.txt")))){
+           Files.createFile(Path.of("readme.txt"));
+       } else {
+           Files.delete(Path.of("readme.txt"));
+       }
 
     }
 }
