2a775c6907de23c94c49182dfa6b73550630e590
mabuttta <mabutta@gmail.com>
2023-05-31 19:28:38 +0300

    ver 1.2

--- src/main/java/Iomain.java
+++ src/main/java/Iomain.java
@@ -1,12 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Iomain {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String s = reader.readLine();
-        System.out.println("Testing message");
-        System.out.println(s);
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        String input;
+
+        while (true){
+            System.out.println("Enter the number: ");
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit"))
+                break;
+
+            try {
+                int num = Integer.parseInt(input);
+                sum+= num;
+            } catch (NumberFormatException e){
+                System.out.println("Error, that "+ input + " not a number");
+            }
+        }
+        System.out.println("Sum of all numbers is: "+sum);
+
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        String s = reader.readLine();
+//        System.out.println("Testing message");
+//        System.out.println(s);
     }
 }
