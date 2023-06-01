c802bec2dc28683c4e2537812896dde3c2f5b581
mabuttta <mabutta@gmail.com>
2023-06-01 19:39:28 +0300

    ver 1.4

---
+++ src/main/java/Homework.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+
+public class Homework {
+    public static void main(String[] args) {
+        File folder = new File("D:\\Новая папка (2)");
+        File[] files = folder.listFiles();
+        for (File file : files
+             ) {
+            System.out.println("Name " + file.getName());
+            System.out.println("Length " + file.length());
+            System.out.println("Is directory " + file.isDirectory());
+            System.out.println("Is file " + file.isFile());
+
+            System.out.println("**************************");
+        }
+
+    }
+}
---
+++ src/main/java/Lessons.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Lessons {
+
+
+ // public static String readLine(String path) throws IOException {
+//   // try с ресурсами для закрытия ресурса
+//      try (BufferedReader br = new BufferedReader(new FileReader(path))){
+//          return br.readLine();
+//      }
+//      // без использования try с ресурсами
+//      BufferedReader br = new BufferedReader(new FileReader(path));
+//      try {
+//          return br.readLine();
+//      } finally {
+//          if (br != null) {
+//              br.close();
+//          }
+//      }
+  }
+
+
