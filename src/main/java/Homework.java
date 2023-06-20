c6f9b63208022eb38bb06c22e5dc7c5e4d966c85
mabuttta <mabutta@gmail.com>
2023-06-20 16:55:30 +0300

    ver 1.4

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -5,19 +5,39 @@ import java.util.Scanner;
 public class Homework {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введи длину строки");
-        int input = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Напечтай произвольный текст");
-        String text = scanner.nextLine();
-        if (text.length()>input){
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("readme.txt"))){
-                    writer.write(text);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        String filename = "readme.txt";
+        try (FileReader reader = new FileReader(filename)) {
+            StringBuilder content = new StringBuilder();
+            int character;
+            int count = 0;
+            while ((character = reader.read()) != -1) {
+                char c = (char) character;
+                content.append(c);
+                if (Character.toLowerCase(c) == 'а') {
+                    count++;
+                }
             }
+            String result = "Количество букв 'а': " + count;
+            try (FileWriter writer = new FileWriter(filename)) {
+                writer.write(result);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введи длину строки");
+//        int input = scanner.nextInt();
+//        scanner.nextLine();
+//        System.out.println("Напечтай произвольный текст");
+//        String text = scanner.nextLine();
+//        if (text.length()>input){
+//            try (BufferedWriter writer = new BufferedWriter(new FileWriter("readme.txt"))){
+//                    writer.write(text);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
 
 
 
