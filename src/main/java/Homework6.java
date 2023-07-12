6637c40305c157a3de99e78c75b2ea10bf5fe308
mabuttta <mabutta@gmail.com>
2023-07-12 19:44:49 +0300

    ver 1.7

--- src/main/java/Homework6.java
+++ src/main/java/Homework6.java
@@ -2,27 +2,31 @@ public class Homework6 {
     private static int i = 0;
 
     public static void main(String[] args) {
+
         Thread increment = new Thread(() -> {
             synchronized (Homework6.class) {
-                i +=10;
+                i += 10;
                 System.out.println("Thread 1: " + i);
                 Homework6.class.notify();
             }
         });
 
         Thread decrement = new Thread(() -> {
-            synchronized (Homework6.class){
+            synchronized (Homework6.class) {
                 try {
                     Homework6.class.wait();
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                    i-=5;
+                i -= 5;
                 System.out.println("Thread 2: " + i);
             }
         });
-        increment.start();
-        decrement.start();
-    }
 
+        decrement.start();
+        increment.start();
+    }
 }
+
+
